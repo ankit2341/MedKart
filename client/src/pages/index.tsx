a785677fcae9a97f988acf892bb49d487ec7a9cd
ankit2341 <ankitpatil2341@gmail.com>
2024-02-22 23:19:52 +0530

    Homepage card designed

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,6 +3,10 @@ import { AppMainLayout } from "@/shared/components/app-layout";
 import {
   Box,
   Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
   Center,
   Flex,
   HStack,
@@ -14,8 +18,11 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCapsules,
+  faCartPlus,
+  faHeart,
   faHouseMedicalCircleCheck,
   faPills,
+  faStar,
   faSuitcaseMedical,
 } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
@@ -307,6 +314,117 @@ const Home = () => {
           </Center>
         </Box>
       </Flex>
+      <Box width="100%" pos="relative">
+        <Heading width="100%" px="10" size="md" textAlign="left">
+          Top Sellers
+        </Heading>
+        <UnderLineAnimation
+          style={{ position: "absolute", top: 1, left: 70 }}
+          id="checkedcircleanimated"
+        />
+      </Box>
+      <Box px={"10"} width="100%" height="50vh" bg="brand.background" mb={10}>
+        <Carousel
+          responsive={responsive}
+          swipeable={isMobile || isTablet ? true : false}
+        >
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((el) => {
+            return (
+              <Card
+                key={el}
+                width="90%"
+                height={"50vh"}
+                borderWidth="1px"
+                borderStyle="solid"
+                borderColor="lightgray"
+                borderRadius={"lg"}
+                background="linear-gradient(207deg, rgba(0,206,209,1) 0%, rgba(19,210,212,1) 30%, rgba(255,255,255,1) 30%)"
+              >
+                <CardHeader
+                  pt={8}
+                  pb={2}
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Image
+                    boxSize="150px"
+                    borderRadius={"lg"}
+                    objectFit="cover"
+                    src="https://d1s24u4ln0wd0i.cloudfront.net/1688202688649fedc050fd2.png"
+                    alt="Dan Abramov"
+                  />
+                  <Flex
+                    pos="absolute"
+                    top={2}
+                    right={2}
+                    borderRadius="md"
+                    align="center"
+                    justify="center"
+                    bg="brand.backgroundDark"
+                    color="brand.background"
+                    px={2}
+                  >
+                    <Text pr={1} fontSize="medium">
+                      4.1
+                    </Text>
+
+                    <FontAwesomeIcon icon={faStar} size="xs" />
+                  </Flex>
+                </CardHeader>
+                <CardBody pt={2}>
+                  <VStack>
+                    <Text fontSize="larger" fontWeight="500" noOfLines={3}>
+                      Riddhish Iron Capsule
+                    </Text>
+                    <HStack>
+                      <Text>₹. 100</Text>
+                      <Text textDecoration="line-through">₹. 150</Text>
+                      <Flex
+                        borderRadius="md"
+                        align="center"
+                        justify="center"
+                        bg="brand.backgroundDark"
+                        color="brand.background"
+                        px={2}
+                      >
+                        <Text pr={1} fontSize="small">
+                          50% off
+                        </Text>
+                      </Flex>
+                    </HStack>
+                  </VStack>
+                </CardBody>
+                <CardFooter>
+                  <Center
+                    width="10"
+                    height={10}
+                    borderRadius="lg"
+                    border="1px solid"
+                    borderColor="brand.primary"
+                  >
+                    <FontAwesomeIcon color="brand.primary" icon={faHeart} />
+                  </Center>
+                  <Button
+                    ml={2}
+                    bg="brand.primary"
+                    color="brand.background"
+                    borderRadius="lg"
+                    width="80%"
+                    height={10}
+                  >
+                    Add to Cart
+                    <FontAwesomeIcon
+                      style={{ paddingLeft: "10px" }}
+                      icon={faCartPlus}
+                    />
+                  </Button>
+                </CardFooter>
+              </Card>
+            );
+          })}
+        </Carousel>
+      </Box>
       <HStack
         spacing={10}
         px={10}
@@ -352,33 +470,112 @@ const Home = () => {
           id="checkedcircleanimated"
         />
       </Box>
-      <Box px={"10"} width="100%" height="50vh" bg="brand.background">
-        <Carousel responsive={responsive} swipeable={true}>
-          <Box width="90%" height={"50vh"} bg="brand.primary"></Box>
-          <Box width="90%" height={"50vh"} bg="brand.primary"></Box>
-          <Box width="90%" height={"50vh"} bg="brand.primary"></Box>
-          <Box width="90%" height={"50vh"} bg="brand.primary"></Box>
-          <Box width="90%" height={"50vh"} bg="brand.primary"></Box>
-          <Box width="90%" height={"50vh"} bg="brand.primary"></Box>
-        </Carousel>
-      </Box>
-      <Box width="100%" pos="relative">
-        <Heading width="100%" px="10" size="md" textAlign="left">
-          Top Sellers
-        </Heading>
-        <UnderLineAnimation
-          style={{ position: "absolute", top: 1, left: 70 }}
-          id="checkedcircleanimated"
-        />
-      </Box>
       <Box px={"10"} width="100%" height="50vh" bg="brand.background" mb={10}>
-        <Carousel responsive={responsive} swipeable={true}>
-          <Box width="90%" height={"50vh"} bg="brand.primary"></Box>
-          <Box width="90%" height={"50vh"} bg="brand.primary"></Box>
-          <Box width="90%" height={"50vh"} bg="brand.primary"></Box>
-          <Box width="90%" height={"50vh"} bg="brand.primary"></Box>
-          <Box width="90%" height={"50vh"} bg="brand.primary"></Box>
-          <Box width="90%" height={"50vh"} bg="brand.primary"></Box>
+        <Carousel
+          responsive={responsive}
+          swipeable={isMobile || isTablet ? true : false}
+        >
+          {[1, 2, 3, 4, 5, 6].map((el) => {
+            return (
+              <Card
+                key={el}
+                width="90%"
+                height={"50vh"}
+                mb={0.2}
+                borderWidth="1px"
+                borderStyle="solid"
+                borderColor="lightgray"
+                borderRadius={"lg"}
+                background="linear-gradient(207deg, rgba(0,206,209,1) 0%, rgba(19,210,212,1) 30%, rgba(255,255,255,1) 30%)"
+              >
+                <CardHeader
+                  pt={8}
+                  pb={2}
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Image
+                    boxSize="150px"
+                    borderRadius={"lg"}
+                    objectFit="cover"
+                    src="https://d1s24u4ln0wd0i.cloudfront.net/1688202688649fedc050fd2.png"
+                    alt="Dan Abramov"
+                  />
+                  <Flex
+                    pos="absolute"
+                    top={2}
+                    right={2}
+                    borderRadius="md"
+                    align="center"
+                    justify="center"
+                    bg="brand.backgroundDark"
+                    color="brand.background"
+                    px={2}
+                  >
+                    <Text pr={1} fontSize="medium">
+                      4.1
+                    </Text>
+
+                    <FontAwesomeIcon icon={faStar} size="xs" />
+                  </Flex>
+                </CardHeader>
+                <CardBody pt={2}>
+                  <VStack>
+                    <Text
+                      fontSize="larger"
+                      textAlign="center"
+                      fontWeight="500"
+                      noOfLines={3}
+                    >
+                      Riddhish Iron Capsule
+                    </Text>
+                    <HStack>
+                      <Text>₹. 100</Text>
+                      <Text textDecoration="line-through">₹. 150</Text>
+                      <Flex
+                        borderRadius="md"
+                        align="center"
+                        justify="center"
+                        bg="brand.backgroundDark"
+                        color="brand.background"
+                        px={2}
+                      >
+                        <Text pr={1} fontSize="small">
+                          50% off
+                        </Text>
+                      </Flex>
+                    </HStack>
+                  </VStack>
+                </CardBody>
+                <CardFooter>
+                  <Center
+                    width="10"
+                    height={10}
+                    borderRadius="lg"
+                    border="1px solid"
+                    borderColor="brand.primary"
+                  >
+                    <FontAwesomeIcon color="brand.primary" icon={faHeart} />
+                  </Center>
+                  <Button
+                    ml={2}
+                    bg="brand.primary"
+                    color="brand.background"
+                    borderRadius="lg"
+                    width="80%"
+                    height={10}
+                  >
+                    Add to Cart
+                    <FontAwesomeIcon
+                      style={{ paddingLeft: "10px" }}
+                      icon={faCartPlus}
+                    />
+                  </Button>
+                </CardFooter>
+              </Card>
+            );
+          })}
         </Carousel>
       </Box>
     </VStack>
--- client/src/shared/components/app-container.tsx
+++ client/src/shared/components/app-container.tsx
@@ -1,15 +1,44 @@
 import { ReactNode } from "react";
 import { Flex } from "@chakra-ui/react";
 import Navbar from "./navigation/navbar";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faBug, faHeadset, faUserShield } from "@fortawesome/free-solid-svg-icons";
 
 interface AppContainerProps {
   children?: ReactNode;
 }
 
+// const SideNav=()=>{
+//   const [showNavbar, setShowNavbar] = useState(false);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       if (window.scrollY > 100) {
+//         setShowNavbar(true);
+//       } else {
+//         setShowNavbar(false);
+//       }
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+
+//   return showNavbar?<VStack zIndex={10000} pos="fixed" bg="white" borderRadius="full" right={0} top={"40vh"} boxShadow={"lg"} border="1px solid" borderColor="brand.primary" spacing={1} px={0} py={4}>
+//     <Button bg="white" color="brand.primary"><FontAwesomeIcon icon={faHeadset} /></Button>
+//     <Button bg="white" color="brand.primary"><FontAwesomeIcon icon={faBug} /></Button>
+//     <Button bg="white" color="brand.primary"><FontAwesomeIcon icon={faUserShield} /></Button>
+//   </VStack>:null;
+// };
+
 export const AppContainer = ({ children }: AppContainerProps) => {
   return (
     <Flex direction="column" minH="100vh">
       <Navbar />
+      {/* <SideNav/> */}
       {children}
     </Flex>
   );
