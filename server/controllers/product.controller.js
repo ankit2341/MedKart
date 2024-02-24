a070f0b4fca6f00b062a900ce0527f49172be30b
ankit2341 <ankitpatil2341@gmail.com>
2024-02-24 19:25:18 +0530

    Product controller started

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -7,9 +7,112 @@ const errorMessage = {
 
 const productController = {
   getProducts: async (req, res) => {
+    const { search, sort, page } = req.query
+    const { filter } = req.body
+    // console.log(search,sort)
+    console.log(filter)
     try {
-      const products = await ProductModel.find()
-      res.status(200).send(products)
+      if (!page || !sort) {
+        res.status(400).send({
+          Message: 'Page is missing',
+        })
+      } else {
+        switch (true) {
+          case search && !filter:
+            const products = await ProductModel.find({
+              $or: [
+                { productName: { $regex: search, $options: 'i' } },
+                { variantPrice: { $regex: search, $options: 'i' } },
+                { variantOldPrice: { $regex: search, $options: 'i' } },
+                { variantOffer: { $regex: search, $options: 'i' } },
+                { rating: { $regex: search, $options: 'i' } },
+                { subtext: { $regex: search, $options: 'i' } },
+                { maker: { $regex: search, $options: 'i' } },
+              ],
+            })
+              .sort({ productName: sort === 'true' ? 1 : -1 })
+              .skip((page - 1) * 10)
+              .limit(10)
+              .collation({ locale: 'en', caseLevel: true })
+            return res.status(200).send(products)
+
+          case !search && filter:
+            console.log(sort, search, filter, page)
+            const variantPricePipeline = [
+              {
+                $addFields: {
+                  variantPriceNumeric: {
+                    $toInt: { $trim: { input: '$variantPrice', chars: '₹ ' } },
+                  },
+                },
+              },
+              {
+                $match: {
+                  variantPriceNumeric: {
+                    $gt: filter?.price ? filter.price : 400,
+                  },
+                },
+              },
+            ]
+
+            // Pipeline for filtering by variantOffer
+            const variantOfferPipeline = [
+              {
+                $addFields: {
+                  variantOfferNumeric: {
+                    $toInt: {
+                      $trim: { input: '$variantOffer', chars: '% off' },
+                    },
+                  },
+                },
+              },
+              {
+                $match: {
+                  variantOfferNumeric: {
+                    $gt: filter?.offer ? filter.offer : 0,
+                  }, // Example: Filter documents where offer is greater than 30%
+                },
+              },
+            ]
+
+            // Pipeline for filtering by rating
+            const ratingPipeline = [
+              {
+                $addFields: {
+                  ratingNumeric: { $toDouble: '$rating' }, // Convert rating to double
+                },
+              },
+              {
+                $match: {
+                  ratingNumeric: { $gt: filter?.rating ? filter.rating : 0 }, // Example: Filter documents where rating is greater than 4.5
+                },
+              },
+            ]
+
+            // Merge pipelines using $facet
+            const productsFiltered = await ProductModel.aggregate([
+              {
+                $facet: {
+                  variantPriceResults: variantPricePipeline,
+                  variantOfferResults: variantOfferPipeline,
+                  ratingResults: ratingPipeline,
+                },
+              },
+            ])
+              .sort({ productName: sort === 'true' ? 1 : -1 })
+              .skip((page - 1) * 10)
+              .limit(10)
+              .collation({ locale: 'en', caseLevel: true })
+            return res.status(200).send(productsFiltered)
+          default:
+            const productsPaginated = await ProductModel.find()
+              .sort({ productName: sort === 'true' ? 1 : -1 })
+              .skip((page - 1) * 10)
+              .limit(10)
+              .collation({ locale: 'en', caseLevel: true })
+            return res.status(200).send(productsPaginated)
+        }
+      }
     } catch {
       res.status(404).send(errorMessage)
     }
--- server/middlewares/authenticate.middleware.js
+++ server/middlewares/authenticate.middleware.js
@@ -1,1 +1,22 @@
-// index
+const jwt = require('jsonwebtoken')
+
+const SECRET_KEY = process.env.secret
+
+const authenticate = (req, res, next) => {
+  const token = req.headers.authorization
+
+  if (token) {
+    const decoded = jwt.verify(token, SECRET_KEY)
+    if (decoded) {
+      next()
+    } else {
+      res.status(401).send({ Message: 'You are not authorized' })
+    }
+  } else {
+    res.status(401).send({ Message: 'You are not Logged in' })
+  }
+}
+
+module.exports = {
+  authenticate,
+}
